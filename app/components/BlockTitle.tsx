adb628e288e728eaeab99ab9778e829f58579ce0
Ayaka <KiritaniAyaka@users.noreply.github.com>
2025-10-26 01:01:29 +0800

    feat: tweak space in title

--- app/components/BlockTitle.tsx
+++ app/components/BlockTitle.tsx
@@ -8,7 +8,7 @@ export default function BlockTitle({ blockNumber, title }: BlockTitleProps) {
   return (
     <h2 className="mb-16 flex items-start text-3xl font-bold text-gray-700">
       <span className="text-6xl">{blockNumber}</span>
-      <span className="font-normal pt-1">{title}</span>
+      <span className="font-normal pt-1 pl-3">{title}</span>
     </h2>
   );
 }
